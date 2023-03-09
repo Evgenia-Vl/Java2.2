a3c3b5ad4b03052cb6de00fb560e999cf188dc2c
EvgeniaVL <e9320915@yandex.ru>
2023-03-09 18:08:26 +0300

    src/

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 100; // начальный остаток на счете
+        int addition = 1000;
+        int bonus = addition / 100;
+        if (addition >= 1000) {
+            System.out.println(bonus = addition / 100);
+        } else {
+            System.out.println(bonus = 0);
+        }
+
+        int finalBalance = balance + addition + bonus;
+        if (bonus < 1000) {
+
+        }
+
+        System.out.println(finalBalance);
+    }
+}
